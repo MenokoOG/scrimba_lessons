32387692ff4bcd2f81e5c16d9b41297aed34e76c
Lawrence Jefferson II <93557311+MenokoOG@users.noreply.github.com>
2023-12-26 16:20:08 -0800

    some more lessons

--- learn_javaScript/js_practice_stuff/rounding_numbers/index.js
+++ learn_javaScript/js_practice_stuff/rounding_numbers/index.js
@@ -3,6 +3,14 @@
 // Don't know which method to use? Google it!
 //do this
 
-const totalPrice = 420.69235632455
-const btn = document.getElementById("purchase-btn")
-btn.textContent = `Buy $${ totalPrice }`
+// const totalPrice = 420.69235632455
+// const btn = document.getElementById("purchase-btn")
+// btn.textContent = `Buy $${ totalPrice.toFixed(2) }`
+
+// Challenge:
+// The toFixed() method doesn't work anymore, it is now a string, Can you make it work?
+// Google the error message if you're unsure about how to do it
+const totalPrice = "420.69235632455"
+const btn = document.getElementById("buy-btn")
+btn.textContent = `Buy $${ Number(totalPrice).toFixed(2) }`
+ 
